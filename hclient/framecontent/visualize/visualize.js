887a658d44ee6eddd60e43244639f76f065651b9
tr4vzlt <travisdoyle123@gmail.com>
2022-08-24 16:59:55 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -503,7 +503,7 @@ function addContainer() {
                        .attr("transform", s);
 
     //var scaleExtentVals = [0.9, 2];
-    var scaleExtentVals = [0.1, 10]; ////[0.75, 7.5]
+    var scaleExtentVals = [0.1, 20]; ////[0.75, 7.5]
 
     if(!settings.isDatabaseStructure){
         scaleExtentVals = [0.5, 3];
