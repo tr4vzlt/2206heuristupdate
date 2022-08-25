5e4e3c7f53fdd1a8fe12f7a18044089b9c9c97a4
tr4vzlt <travisdoyle123@gmail.com>
2022-08-25 14:18:39 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -504,11 +504,12 @@ function addContainer() {
                        .attr("id", "container")
                        .attr("transform", s);
 
-    //var scaleExtentVals = [0.9, 2]; ////[0.75, 7.5]
-    var scaleExtentVals = [0.1, 10];
+    var scaleExtentVals = [0.9, 2]; ////[0.75, 7.5]
+    
 
     if(!settings.isDatabaseStructure){
-        scaleExtentVals = [0.5, 3];
+        //scaleExtentVals = [0.5, 3];
+        scaleExtentVals = [0.1, 30];
     }
 
     // Zoom behaviour                   
