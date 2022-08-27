1c01b7a54b2ab11509e8be12f40123a202218c0a
GerardHolmanLee <gerardhl099@gmail.com>
2022-08-27 12:40:34 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -1514,7 +1514,7 @@ function showEmbedDialog(){
 }
 
 function openWin() {
-    var divText = document.getElementById("newOne").outerHTML;
+    var divText = document.getElementById("divSvg").outerHTML;
     var myWindow = window.open('', '', 'width=200,height=100');
     var doc = myWindow.document;
     doc.open();
