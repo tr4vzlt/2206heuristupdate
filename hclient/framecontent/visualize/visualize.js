24b7693c841579dad3b494bfd292c4815cbe1d4b
GerardHolmanLee <gerardhl099@gmail.com>
2022-08-29 13:34:23 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -1477,6 +1477,12 @@ function addLabels(name, color) {
     return labels;
 }
 
+function removeNames() {
+    var labels = d3.selectAll(".node").append(NULL);
+
+    return labels;
+}
+
 //
 //
 //
