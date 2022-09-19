3589b454da2e0ddd86cb12c18eafbbee63030420
tr4vzlt <travisdoyle123@gmail.com>
2022-09-19 20:41:51 +1000

    Update drag.js

--- hclient/framecontent/visualize/drag.js
+++ hclient/framecontent/visualize/drag.js
@@ -34,9 +34,10 @@ function addNodes() {
                   .data(data.nodes)
                   .enter()
                   .append("g")
-                  .on("dblclick",(event) => console.log('dblclick evt', event));
                     //Added Double Click to Edit Function - TD 19/9/2022
-                          //window.open(window.hWin.HAPI4.baseURL +'?fmt=edit&db='+window.hWin.HAPI4.database+'&recID='+d.id, '_blank');
+                  .on("dblclick",(d) => window.open(window.hWin.HAPI4.baseURL +'?fmt=edit&db='+window.hWin.HAPI4.database+'&recID='+d.id, '_blank'));
+                    
+                          
                   
 
     // Dragging
