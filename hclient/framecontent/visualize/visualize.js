be72afab11e867c454d76176bddb9b1faa10e8ad
tr4vzlt <travisdoyle123@gmail.com>
2022-09-19 14:44:05 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -1645,7 +1645,7 @@ function restartPage(){
 
 
 function openWin() {
-    var newWindowContent = document.getElementById("entirenetwork").innerHTML;
+    var newWindowContent = openfile(springDiagram.php);
     var newWindow = window.open('', '', 'width=1920,height=1080');
     newWindow.document.write(newWindowContent);
   }
