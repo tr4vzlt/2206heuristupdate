8f16923b6aa99e80edb271b5c17fd5b39e5e55d3
tr4vzlt <travisdoyle123@gmail.com>
2022-09-26 20:31:38 +1000

    Update visualize.js

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -1555,7 +1555,7 @@ function openWin() {
 
     var url2 = window.hWin.HAPI4.baseURL + 'hclient/framecontent/visualize/springDiagram.php' + hrefnew;
 
-    if (fullscreen){
+    if (fullscreen = true){
         full_btn.style.visibility = 'hidden';
         cls_button.style.visibility = 'visible';
     }
