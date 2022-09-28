1ef59e1cd68629ec22da19e7070ae5149314740e
GerardHolmanLee <gerardhl099@gmail.com>
2022-09-28 12:56:54 +1000

    Text resize

--- hclient/framecontent/visualize/visualize.js
+++ hclient/framecontent/visualize/visualize.js
@@ -1560,3 +1560,27 @@ function openWin() {
 
 
 }
+
+const [viewportWidth,fontSizeRem,fontSizePx] = document.querySelectorAll("nodelabel.namelabel");
+
+function calculate(){
+  viewportWidth.innerText = `Viewport Width: ${window.innerWidth}px`;
+  
+  fontSizeRem.innerText = `Font Size (rem): ${Number(window.getComputedStyle(viewportWidth).fontSize.slice(0, -2)) / 16}rem`;
+  
+  fontSizePx.innerText = `Font Size (px): ${window.getComputedStyle(viewportWidth).fontSize}`;
+  
+  if(window.innerWidth >= 840 || window.innerWidth <= 360){
+    fontSizePx.classList.add("red");
+    fontSizeRem.classList.add("red");
+  }else{
+    fontSizePx.classList.remove("red");
+    fontSizeRem.classList.remove("red");
+  }
+}
+
+window.addEventListener("resize", () => {
+  calculate();
+});
+
+calculate();
