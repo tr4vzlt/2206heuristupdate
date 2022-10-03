015eaffeb163c09a169b61a78a23858d32819464
ISHMAMUL <hawkbd99@gmail.com>
2022-10-03 18:51:56 +1100

    Update drag.js

--- hclient/framecontent/visualize/drag.js
+++ hclient/framecontent/visualize/drag.js
@@ -160,7 +160,10 @@ function showNodeInformation(d){
     var infoBox = d3.select("#infoBox");//select infobox
     infoBox.selectAll("h2").remove();//remove header
     infoBox.selectAll("p").remove();//remove paragraph
-    console.log("Data id " + d[2]);
+    console.log(d.name);
+    
+    //Access info in the d object;
+    
     
     //get data from node
     //console.log("info" + d.datum());
