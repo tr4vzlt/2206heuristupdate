2864a304a3417624d339dec271d4f57d331f41e4
GerardHolmanLee <gerardhl099@gmail.com>
2022-10-14 17:26:42 +1100

    Update drag.js

--- hclient/framecontent/visualize/drag.js
+++ hclient/framecontent/visualize/drag.js
@@ -96,6 +96,7 @@ function addNodes() {
         //add internal circle
         node.append("circle")
             .attr("r", circleSize)
+            console.log(d)
             .attr("class", 'foreground icon-foreground yellowClass')
             .attr("fill", 'blue')
             .style({"stroke": "#ddd", 'display': icon_display})
