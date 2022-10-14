c6b28526da91d45df7e36b17d9a9563ae6f38cff
ISHMAMUL <hawkbd99@gmail.com>
2022-10-14 17:41:58 +1100

    Update drag.js

--- hclient/framecontent/visualize/drag.js
+++ hclient/framecontent/visualize/drag.js
@@ -183,7 +183,7 @@ function showNodeInformation(d){
     console.log("src text = " + srcURL); 
     var iframeDiv = d3.select("#iframeDiv");//select the parent div
     var infoBox = d3.select("#iframeInfo");//select the iframe
-    var recordId = d3.select("#recID");
+    var recordId = document.getElementById('recID').style.display = "none";
     infoBox.attr("src", srcURL);//supply document to iframe
     iframeDiv.style("display", "block");//make iframe visible
     recordId.style("display", "none");//hide record ID
