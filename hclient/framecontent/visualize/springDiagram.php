94faa603d0fd73403a85d0fe458c45b54254915d
JamesYan012 <112322607+JamesYan012@users.noreply.github.com>
2022-10-17 12:28:30 +1100

    1

--- hclient/framecontent/visualize/springDiagram.php
+++ hclient/framecontent/visualize/springDiagram.php
@@ -282,7 +282,7 @@ function onPageInit(success){
         function onVisualizeResize(){
                 var width = $(window).width();
                 var supw = (width<744)?3.8:0; //1120
-                //$('#divSvg').css('top', 5+supw+'em');
+                $('#divSvg').css('top', 0);
         }
             
         </script>
