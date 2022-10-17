2b84552886acea2db1a91fe5488fec44d7b26a6a
tr4vzlt <travisdoyle123@gmail.com>
2022-10-17 18:32:25 +1100

    Update overlay.js

--- hclient/framecontent/visualize/overlay.js
+++ hclient/framecontent/visualize/overlay.js
@@ -732,7 +732,7 @@ function createOverlay(x, y, type, selector, node_obj, parent_node) {
                     tick();
                 });
       
-      if( settings.isDatabaseStructure ||  settings.onRefreshData){
+      if( !settings.isDatabaseStructure ||  settings.onRefreshData){
       
           //link button      
           var btnAddLink = overlay
@@ -824,7 +824,7 @@ function createOverlay(x, y, type, selector, node_obj, parent_node) {
         }else{ // disabled buttons
 
             btnEdit.style('display', 'none');
-            //btnAddLink.style('display', 'none');
+            btnAddLink.style('display', 'none');
         }
       }else{ // add edit button
         
