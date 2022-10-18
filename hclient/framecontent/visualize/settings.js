202d9e03ad00f89120c3229064eab9aecc71a060
JamesYan012 <112322607+JamesYan012@users.noreply.github.com>
2022-10-19 06:20:10 +1100

    Update settings.js

--- hclient/framecontent/visualize/settings.js
+++ hclient/framecontent/visualize/settings.js
@@ -625,3 +625,22 @@ function setLinkMode(formula) {
     visualizeData();
     _syncUI();
 }
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
